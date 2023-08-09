669737593c022f6a0baaea3acf8e4679109a3e2f
Daniel Amaya <damayalop@gmail.com>
2023-08-08 22:52:21 -0600

    📝 Update title

--- src/components/TodoContainer/index.jsx
+++ src/components/TodoContainer/index.jsx
@@ -35,7 +35,7 @@ export default function TodoContainer() {
   return (
     <div className="flex flex-col gap-4 w-full px-3 transition-all duration-300 sm:w-[31.875rem]">
       <h1 className="font-bold text-4xl text-center dark:text-white">
-        Todo APP
+        TODO APP
       </h1>
 
       <TodoSearch handlerSearchTodo={setSearchText} />
